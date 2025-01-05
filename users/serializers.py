7c862088bdcdf63e3c5532699a2814e36c8e1048
MihGen <genziks@inbox.lv>
2025-01-06 01:28:32 +0200

    V1

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['email'],
+            phone=validated_data['phone'],
+            password=validated_data['password'],
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone', 'password')
