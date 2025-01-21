fcedb3d5d04e5950c457e7396c1a9aba7e7afeea
MihailGen <166643818+MihailGen@users.noreply.github.com>
2025-01-21 07:49:48 +0200

    Delete Create_volume.sh

--- Create_volume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Пример скрипта для создания независимого volume с данными
-
-docker volume create user_data:/var/lib/postgresql/data
-
-docker run --rm -v user_data:/var/lib/postgresql/data -v /backup:/backup busybox cp -a /backup/. /var/lib/postgresql/data
