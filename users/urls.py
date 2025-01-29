fe318795a7f4b3ca2acb8838a8c54c9f8be43cf3
MihGen <genziks@inbox.lv>
2025-01-29 22:51:36 +0200

    v8

--- users/urls.py
+++ users/urls.py
@@ -1,53 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, home, SignUp
-from datetime import time
+from .views import RegisterView, home, SignUp, metrics, compute, heavy_compute
 
-# ----------------- Prometheus encapsulate --------------------------
-
-from fastapi import FastAPI, Request
-from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response
-
-app = FastAPI()
-
-REQUEST_COUNT = Counter('request_count', 'Total number of requests', ['UsersList', 'endpoint'])
-REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency', ['UsersList', 'endpoint'])
-
-
-class MetricsMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        endpoint = request.url.path
-        REQUEST_COUNT.labels(app_name='UsersList', endpoint=endpoint).inc()
-        with REQUEST_LATENCY.labels(app_name='UsersList', endpoint=endpoint).time():
-            response = await call_next(request)
-        return response
-
-
-app.add_middleware(MetricsMiddleware)
-
-
-@app.get('/metrics')
-async def metrics():
-    data = generate_latest()
-    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
-
-
-@app.get('/compute')
-def compute():
-    time.sleep(2)
-    return {"message": "Completed a complex computation"}
-
-
-@app.get('/heavy_compute')
-def heavy_compute():
-    for t in range(150):
-        time.sleep(2)
-    return {"message": "Completed a series of computations"}
-
-
-# --------------- Prometheus End ---------------------------------
 
 
 urlpatterns = [
@@ -57,8 +11,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('django.contrib.auth.urls')),
-    #path('metrics/', metrics(), name='prometheus_metrics'),
-    #path('compute/', compute(), name='prometheus_compute'),
-    #path('heavy_compute/', heavy_compute(), name='prometheus_heavy_compute'),
+    #path('metrics/', metrics, name='prometheus_metrics'),
+    #path('compute/', compute, name='prometheus_compute'),
+    #path('heavy_compute/', heavy_compute, name='prometheus_heavy_compute'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -15,7 +15,53 @@ from .models import User
 from .permissions import CanEdit
 from .serializers import RegisterSerializer
 
+from datetime import time
 
+# ----------------- Prometheus encapsulate --------------------------
+
+from fastapi import FastAPI, Request
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+app = FastAPI()
+
+REQUEST_COUNT = Counter('request_count', 'Total number of requests', ['UsersList', 'endpoint'])
+REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency', ['UsersList', 'endpoint'])
+
+
+class MetricsMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        endpoint = request.url.path
+        REQUEST_COUNT.labels(app_name='UsersList', endpoint=endpoint).inc()
+        with REQUEST_LATENCY.labels(app_name='UsersList', endpoint=endpoint).time():
+            response = await call_next(request)
+        return response
+
+
+app.add_middleware(MetricsMiddleware)
+
+
+@app.get('/metrics')
+async def metrics():
+    data = generate_latest()
+    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
+
+
+@app.get('/compute')
+def compute():
+    time.sleep(2)
+    return {"message": "Completed a complex computation"}
+
+
+@app.get('/heavy_compute')
+def heavy_compute():
+    for t in range(150):
+        time.sleep(2)
+    return {"message": "Completed a series of computations"}
+
+
+# --------------- Prometheus End ---------------------------------
 
 
 
